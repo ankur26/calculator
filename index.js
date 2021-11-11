2b126e61b0be2a8c4b1c3c1ffedf4e5ad84ee0ed
Ankur Bhatkalkar <ab7869@nyu.edu>
2021-11-11 00:04:02 -0500

    added calculation logic without = operator

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const add = (a,b) => {return a+b};
 const subtract = (a,b) => {return a-b};
 const divide = (a,b) => { return a/b};
 const multiply = (a,b) => { return a*b};
-
 const operate = (a,b,operator) => {
     switch(operator){
         case "+":
@@ -20,3 +19,45 @@ const operate = (a,b,operator) => {
             break;
     }
 }
+const numbers = "1234567890.";
+const operators = "+/*-r";
+let calculationStack = "";
+const incompleteAnswer = document.getElementById("incomplete-answer");
+const currentAction = document.getElementById("current-action");
+const operation = document.getElementById("operation");
+const buttons = document.querySelectorAll("button");
+const reset = () => {
+    incompleteAnswer.textContent = "";
+    currentAction.textContent = "";
+    operation.textContent = "";
+    calculationStack = "";
+}
+
+const clickHandler=(e)=>{
+    // console.log(e.target)
+    if(numbers.includes(e.target.value)){
+        currentAction.textContent+=e.target.value;
+    }
+    else if(operators.includes(e.target.value)){
+        if(e.target.value === "r") reset();
+        else{
+            let currentValue = currentAction.textContent.includes(".")? currentAction.textContent * 1.0 : parseInt(currentAction.textContent);
+            if (incompleteAnswer.textContent === ""){
+                incompleteAnswer.textContent = currentValue;
+
+            }else{
+                let prevValue = incompleteAnswer.textContent.includes(".") ? incompleteAnswer.textContent * 1.0 : parseInt(incompleteAnswer.textContent);
+                incompleteAnswer.textContent = operate(currentValue,prevValue,calculationStack);
+            }
+            currentAction.textContent = "";
+            calculationStack=e.target.value;
+            operation.textContent = calculationStack;
+        }
+    }
+}
+
+buttons.forEach(button=>{button.addEventListener('click',clickHandler)});
+
+
+
+reset();
