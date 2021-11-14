ce323d8bc9b0ff22cba55bfc1558b4d8d4fcf20a
Ankur Bhatkalkar <ab7869@nyu.edu>
2021-11-14 18:58:01 -0500

    Fixed logic and added complete = and backspace implementation

--- index.js
+++ index.js
@@ -1,100 +1,154 @@
-const add = (a,b) => {return a+b};
-const subtract = (a,b) => {return a-b};
-const divide = (a,b) => { return a/b};
-const multiply = (a,b) => { return a*b};
-const operate = (a,b,operator) => {
-    switch(operator){
-        case "+":
-            return add(a,b);
-        case "-":
-            return subtract(a,b);
-        case "/":
-            return divide(a,b);
-        case "*":
-            return multiply(a,b);
-        default:
-            return "Ideally to be not reached.";
-            break;
-    }
-}
-const numbers = "1234567890.";
-const operators = "+/*-=br";
-let warningFlag = false;
-let calculationStack = "";
+const add = (a, b) => {
+	return a + b;
+};
+const subtract = (a, b) => {
+	return a - b;
+};
+const divide = (a, b) => {
+	return a / b;
+};
+const multiply = (a, b) => {
+	return a * b;
+};
+const operate = (a, b, operator) => {
+	switch (operator) {
+		case '+':
+			return add(a, b);
+		case '-':
+			return subtract(a, b);
+		case '/':
+			return divide(a, b);
+		case '*':
+			return multiply(a, b);
+		default:
+			return 'Ideally to be not reached.';
+			break;
+	}
+};
+const numbers = '1234567890.';
+const operators = '+/*-=br';
+let numbersDisabled = false;
+let calculationStack = '';
 let operatorsDisabled = false;
-const incompleteAnswer = document.getElementById("incomplete-answer");
-const currentAction = document.getElementById("current-action");
-const operation = document.getElementById("operation");
-const buttons = document.querySelectorAll("button");
-const warning = document.getElementById("warning");
-const disableNumbersAndWarn = () =>{
-    buttons.forEach(button =>{
-        if(numbers.includes(`${button.value}`)){
-            button.classList.toggle("disabled")
-        }
-    });
-    warning.classList.toggle("invisible");
-    warningFlag = true;
-}
-const disableOperators = () => {
-    buttons.forEach(button =>{
-        if(operators.includes(`${button.value}`)){
-            button.classList.toggle("disabled")
-        }
-    });
-    operatorsDisabled = true;
-}
+let equalsClicked = false;
+// let dotDisabled = false;
+const firstNumber = document.getElementById('firstNumber');
+const secondNumber = document.getElementById('secondNumber');
+const operation = document.getElementById('operation');
+const buttons = document.querySelectorAll('button');
+const warning = document.getElementById('warning');
+const dotButton = document.getElementById('dot');
+// const doCalculatorValidations = () => {
+//     if
+//
+const disableNumbers = () => {
+	warning.classList.toggle('invisible');
+	buttons.forEach((button) => {
+		if (numbers.includes(button.value)) {
+			// if(button.value === "." && dotDisabled) continue;
+			button.classList.toggle('disabled');
+		}
+	});
+	numbersDisabled = true;
+};
+const checkDot = () => {
+	if (secondNumber.textContent.includes('.')) {
+		dotButton.classList.add('disabled');
+		// dotDisabled = true;
+	} else {
+		dotButton.classList.remove('disabled');
+	}
+};
+const parseValue = (number) => {
+	return number.includes('.') ? number * 1.0 : parseInt(number);
+};
+const checkNumbers = () => {
+	if (firstNumber.textContent === '' && secondNumber.textContent === '') {
+		buttons.forEach((button) => {
+			if (operators.includes(button.value)) {
+				button.classList.add('disabled');
+			}
+		});
+	} else {
+		buttons.forEach((button) => {
+			if (operators.includes(button.value)) {
+				button.classList.remove('disabled');
+			}
+		});
+	}
+};
+const clickHandler = (e) => {
+    if(equalsClicked){equalsClicked=false;reset();}
+	let current = e.target.value;
+	if (numbers.includes(current)) {
+		secondNumber.textContent += current;
+		if (secondNumber.textContent.length > 20) {
+			disableNumbers();
+		}
+		checkDot();
+	} else if (operators.includes(current)) {
+		// console.log("Operator");
+		if (current === 'r') {
+			reset();
+		} else if (current === 'b') {
+			secondNumber.textContent = secondNumber.textContent.substr(0, secondNumber.textContent.length - 1);
+			checkDot();
+		} else {
+			if (current === '=') {
+				// To be implemented later
+				if (operation.textContent) {
+					if (secondNumber.textContent === '') {
+						secondNumber.textContent = firstNumber.textContent;
+                        firstNumber.textContent = "";
+					} else {
+						let result = operate(
+							parseValue(firstNumber.textContent),parseValue(secondNumber.textContent), operation.textContent
+						);
+						secondNumber.textContent = result;
+						firstNumber.textContent = '';
+					}
+				}
+                equalsClicked = true;
+				operation.textContent = '';
+			} else {
+				if (firstNumber.textContent === '') {
+					firstNumber.textContent = secondNumber.textContent;
+					secondNumber.textContent = '';
+					operation.textContent = current;
+				} else {
+					if (secondNumber.textContent) {
+						let firstNum = parseValue(firstNumber.textContent);
+						let secondNum = parseValue(secondNumber.textContent);
+						if (operation.textContent !== '') {
+							let result = operate(firstNum, secondNum, operation.textContent);
+							firstNumber.textContent = result;
+							operation.textContent = current;
+							secondNumber.textContent = '';
+						}
+					} else {
+						operation.textContent = current;
+					}
+				}
+			}
+		}
+	}
+	checkNumbers();
+};
 const reset = () => {
-    incompleteAnswer.textContent = "";
-    currentAction.textContent = "";
-    operation.textContent = "";
-    calculationStack = "";
-    if(warningFlag){
-        disableNumbersAndWarn();
-        warningFlag = false;
-    }
-    disableOperators();
-}
+	firstNumber.textContent = '';
+	secondNumber.textContent = '';
+	operation.textContent = '';
+	dotButton.classList.remove('disabled');
+	buttons.forEach((button) => {
+		button.classList.remove('disabled');
+	});
+	checkNumbers();
+};
 
-const clickHandler=(e)=>{
-    // console.log(e.target)
-    if(numbers.includes(e.target.value)){
-        currentAction.textContent+=e.target.value;
-        if(currentAction.textContent.length > 0 && operatorsDisabled){disableOperators();operatorsDisabled = false};
-        if(currentAction.textContent.length > 20 && !warningFlag){
-            // currentAction.textContent = "Char limit reached";
-            disableNumbersAndWarn();
-        }
-        
-    }
-    else if(operators.includes(e.target.value)){
-        if(e.target.value === "r") reset();
-        else if(e.target.value === "b") {
-            currentAction.textContent = currentAction.textContent.substr(0,currentAction.textContent.length-1);
-            if(warningFlag){
-                disableNumbersAndWarn();
-                warningFlag = false;
-            }
-        }
-        else{
-            let currentValue = currentAction.textContent.includes(".")? currentAction.textContent * 1.0 : parseInt(currentAction.textContent)
-            if (incompleteAnswer.textContent === ""){
-                incompleteAnswer.textContent = currentValue;
+buttons.forEach((button) => {
+	button.addEventListener('click', clickHandler);
+});
 
-            }else{
-                let prevValue = incompleteAnswer.textContent.includes(".") ? incompleteAnswer.textContent * 1.0 : parseInt(incompleteAnswer.textContent);
-                incompleteAnswer.textContent = operate(currentValue,prevValue,calculationStack);
-            }
-            currentAction.textContent = "";
-            calculationStack=e.target.value;
-            operation.textContent = calculationStack;
-        }
-        if(currentAction.textContent.length === 0){disableOperators();};
-    }
-}
-
-buttons.forEach(button=>{button.addEventListener('click',clickHandler)});
-
-
+// buttons.forEach(button=>{button.addEventListener('click',clickHandler)});
 
 reset();
